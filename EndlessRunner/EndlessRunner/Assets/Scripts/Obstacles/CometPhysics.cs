50440d58c2f7e3fd212c08ee9df4088c9b5d622c
INTingKING <82975137+INTingKING@users.noreply.github.com>
2023-07-29 01:01:11 +0200

    Comet_physics_for_starters
    LAHWBLDIABWL

---
+++ EndlessRunner/EndlessRunner/Assets/Scripts/Obstacles/CometPhysics.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
+
+public class CometPhysics : MonoBehaviour
+{
+    public LayerMask ground;
+    public LayerMask player;
+
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float directionX = 0;
+    //public float directionX = -5f;
+
+    public float timer = 0;
+
+    public bool useMethodWaitBeforeFall = true;
+    public bool addForce = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        rb.gravityScale = 0;
+        directionX = Random.Range(-0.025f, 0.025f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useMethodWaitBeforeFall)
+        {
+            WaitBeforeFall();
+        }
+        if (addForce)
+        {
+            Fall();
+
+        }
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    void WaitBeforeFall()
+    {
+        if (timer <= 2.5f)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 2.5f)
+        {
+            rb.gravityScale = 1;
+
+            useMethodWaitBeforeFall = false;
+            addForce = true;
+        }
+    }
+
+    void Fall()
+    {
+        rb.AddForce(Vector2.right * directionX, ForceMode2D.Impulse);
+    }
+
+}
