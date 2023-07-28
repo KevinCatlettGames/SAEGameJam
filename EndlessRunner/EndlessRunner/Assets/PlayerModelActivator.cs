cced3d8a3114c1dfd1b120dda2304a2ea10db00c
Kevin Catlett <kevin.catlett@terovania.de>
2023-07-28 22:59:36 +0200

    Player essentials

---
+++ EndlessRunner/EndlessRunner/Assets/PlayerModelActivator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PlayerModelActivator : MonoBehaviour
+{
+
+    public GameObject modelObject;
+    public GameObject spriteObject;
+
+    private void Awake()
+    {
+    }
+}
