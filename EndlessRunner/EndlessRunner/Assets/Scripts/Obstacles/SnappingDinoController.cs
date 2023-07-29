e19a9bce8330d104a241c7d03c7e6f58c232e004
Kevin Catlett <kevin.catlett@terovania.de>
2023-07-29 11:39:05 +0200

    Snapping DIno and IObstacle Interface

--- EndlessRunner/EndlessRunner/Assets/Scripts/Obstacles/SnappingDinoController.cs
+++ EndlessRunner/EndlessRunner/Assets/Scripts/Obstacles/SnappingDinoController.cs
@@ -4,28 +4,17 @@ using UnityEngine;
 /// The snapping dino plays a snapping animation and activates appropriate colliders depending on the current frame.
 /// The snapping animation is started after a random time, <see cref="minMaxIdleTime"/>.
 /// </summary>
-public class SnappingDinoController : MonoBehaviour
+public class SnappingDinoController : MonoBehaviour, IObstacle
 {   
     [SerializeField] Animator animator;
-    [SerializeField] Vector2 minMaxIdleTime = new Vector2(3, 5);
+    bool snapped; 
 
-    float idleTime;  
-
-    private void Awake()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        InitializeIdleTime();
-    }
-    private void Update()
-    {
-        idleTime -= Time.deltaTime;
-        if(idleTime <= 0)
+        if(collision.CompareTag("Player") && !snapped)
         {
+            snapped = true;
             animator.SetTrigger("Snap");
-            InitializeIdleTime();
         }
-    }
-    private void InitializeIdleTime()
-    {
-        idleTime = Random.Range(minMaxIdleTime.x, minMaxIdleTime.y);
     }
 }
